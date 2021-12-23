4a642ab745cc18627407bd21125b9fe1ac25ebec
MoonRocket <94447633+moonrock-et@users.noreply.github.com>
2021-12-23 17:56:54 +0100

    dev

--- frontend/components/sections/horizontal-scroll-sections.js
+++ frontend/components/sections/horizontal-scroll-sections.js
@@ -1,106 +1,107 @@
-import ScrollHorizontal from "../elements/scroll-horizontal";
+// import ScrollHorizontal from "../elements/scroll-horizontal";
+import ScrollHorizontal from "utils/scroll-horizontal";
 
 const HorizontalScrollSections = ({ data }) => {
   return (
     <>
       <section className="daaaltocode">
-        <ScrollHorizontal>
-          <div className="altocode bg-yellow-10">
-            <div className="container">
-              <div className="flex md:gap-90">
-                <div className="flex flex-col">
-                  <p>1</p>
-                  <p>
-                    La magie se produit à l’intersection. Nous comprenons les
-                    interactions entre les choses et réalisons les projets du
-                    début à la fin en combinant de manière holistique les
-                    domaines d'activité du pilotage de chantier, de la gestion
-                    administrative, de l'architecture, de la construction, de
-                    l’aménagement, du design d'intérieur et du mobilier. Le tout
-                    dans un seul contrat.
-                  </p>
-                </div>
-                <div className="flex flex-col"></div>
+        {/* <ScrollHorizontal> */}
+        <div className="altocode bg-yellow-10">
+          <div className="container">
+            <div className="flex md:gap-90">
+              <div className="flex flex-col">
+                <p>1</p>
+                <p>
+                  La magie se produit à l’intersection. Nous comprenons les
+                  interactions entre les choses et réalisons les projets du
+                  début à la fin en combinant de manière holistique les domaines
+                  d'activité du pilotage de chantier, de la gestion
+                  administrative, de l'architecture, de la construction, de
+                  l’aménagement, du design d'intérieur et du mobilier. Le tout
+                  dans un seul contrat.
+                </p>
               </div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
-          <div className="altocode bg-brown-light">
-            <div className="container">
-              <div className="flex md:gap-90">
-                <div className="flex flex-col">
-                  <p>1</p>
-                  <p>
-                    La magie se produit à l’intersection. Nous comprenons les
-                    interactions entre les choses et réalisons les projets du
-                    début à la fin en combinant de manière holistique les
-                    domaines d'activité du pilotage de chantier, de la gestion
-                    administrative, de l'architecture, de la construction, de
-                    l’aménagement, du design d'intérieur et du mobilier. Le tout
-                    dans un seul contrat.
-                  </p>
-                </div>
-                <div className="flex flex-col"></div>
+        </div>
+        <div className="altocode bg-brown-light">
+          <div className="container">
+            <div className="flex md:gap-90">
+              <div className="flex flex-col">
+                <p>1</p>
+                <p>
+                  La magie se produit à l’intersection. Nous comprenons les
+                  interactions entre les choses et réalisons les projets du
+                  début à la fin en combinant de manière holistique les domaines
+                  d'activité du pilotage de chantier, de la gestion
+                  administrative, de l'architecture, de la construction, de
+                  l’aménagement, du design d'intérieur et du mobilier. Le tout
+                  dans un seul contrat.
+                </p>
               </div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
-          <div className="altocode bg-brown">
-            <div className="container">
-              <div className="flex md:gap-90">
-                <div className="flex flex-col">
-                  <p>1</p>
-                  <p>
-                    La magie se produit à l’intersection. Nous comprenons les
-                    interactions entre les choses et réalisons les projets du
-                    début à la fin en combinant de manière holistique les
-                    domaines d'activité du pilotage de chantier, de la gestion
-                    administrative, de l'architecture, de la construction, de
-                    l’aménagement, du design d'intérieur et du mobilier. Le tout
-                    dans un seul contrat.
-                  </p>
-                </div>
-                <div className="flex flex-col"></div>
+        </div>
+        <div className="altocode bg-brown">
+          <div className="container">
+            <div className="flex md:gap-90">
+              <div className="flex flex-col">
+                <p>1</p>
+                <p>
+                  La magie se produit à l’intersection. Nous comprenons les
+                  interactions entre les choses et réalisons les projets du
+                  début à la fin en combinant de manière holistique les domaines
+                  d'activité du pilotage de chantier, de la gestion
+                  administrative, de l'architecture, de la construction, de
+                  l’aménagement, du design d'intérieur et du mobilier. Le tout
+                  dans un seul contrat.
+                </p>
               </div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
-          <div className="altocode bg-green">
-            <div className="container">
-              <div className="flex md:gap-90">
-                <div className="flex flex-col">
-                  <p>1</p>
-                  <p>
-                    La magie se produit à l’intersection. Nous comprenons les
-                    interactions entre les choses et réalisons les projets du
-                    début à la fin en combinant de manière holistique les
-                    domaines d'activité du pilotage de chantier, de la gestion
-                    administrative, de l'architecture, de la construction, de
-                    l’aménagement, du design d'intérieur et du mobilier. Le tout
-                    dans un seul contrat.
-                  </p>
-                </div>
-                <div className="flex flex-col"></div>
+        </div>
+        <div className="altocode bg-green">
+          <div className="container">
+            <div className="flex md:gap-90">
+              <div className="flex flex-col">
+                <p>1</p>
+                <p>
+                  La magie se produit à l’intersection. Nous comprenons les
+                  interactions entre les choses et réalisons les projets du
+                  début à la fin en combinant de manière holistique les domaines
+                  d'activité du pilotage de chantier, de la gestion
+                  administrative, de l'architecture, de la construction, de
+                  l’aménagement, du design d'intérieur et du mobilier. Le tout
+                  dans un seul contrat.
+                </p>
               </div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
-          <div className="altocode bg-grey">
-            <div className="container">
-              <div className="flex md:gap-90">
-                <div className="flex flex-col">
-                  <p>1</p>
-                  <p>
-                    La magie se produit à l’intersection. Nous comprenons les
-                    interactions entre les choses et réalisons les projets du
-                    début à la fin en combinant de manière holistique les
-                    domaines d'activité du pilotage de chantier, de la gestion
-                    administrative, de l'architecture, de la construction, de
-                    l’aménagement, du design d'intérieur et du mobilier. Le tout
-                    dans un seul contrat.
-                  </p>
-                </div>
-                <div className="flex flex-col"></div>
+        </div>
+        <div className="altocode bg-grey">
+          <div className="container">
+            <div className="flex md:gap-90">
+              <div className="flex flex-col">
+                <p>1</p>
+                <p>
+                  La magie se produit à l’intersection. Nous comprenons les
+                  interactions entre les choses et réalisons les projets du
+                  début à la fin en combinant de manière holistique les domaines
+                  d'activité du pilotage de chantier, de la gestion
+                  administrative, de l'architecture, de la construction, de
+                  l’aménagement, du design d'intérieur et du mobilier. Le tout
+                  dans un seul contrat.
+                </p>
               </div>
+              <div className="flex flex-col"></div>
             </div>
           </div>
-        </ScrollHorizontal>
+        </div>
+        {/* </ScrollHorizontal> */}
       </section>
     </>
   );
