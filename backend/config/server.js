be23070a2d892612837a6e8aaa8e1c36421dd3f3
MoonRocket <94447633+moonrock-et@users.noreply.github.com>
2022-01-03 11:51:28 +0100

    fix server.js & api-utils error

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: aalto-groupe.fr,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
 });
